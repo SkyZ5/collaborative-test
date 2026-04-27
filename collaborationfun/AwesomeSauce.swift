2c24dd546e1f1362e8654503bb6da5df7b7e23bb
BT_C09_14 <Hp5680@bths.edu>
2026-04-27 13:35:12 -0400

    I became lost in the sauce

---
+++ collaborationfun/AwesomeSauce.swift
@@ -0,0 +1,20 @@
+//
+//  AwesomeSauce.swift
+//  collaborationfun
+//
+//  Created by Student on 4/27/26.
+//
+
+import SwiftUI
+
+struct AwesomeSauce: View {
+    var body: some View {
+        Text("Mayonnaise")
+        Text("Ketchup")
+        Text("Mustard")
+    }
+}
+
+#Preview {
+    AwesomeSauce()
+}
